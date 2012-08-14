0fdbac9c839e3762215646fb0b2e9067e0d8f0eb
Pierre-Alexandre Meyer <pierre@ning.com>
2012-08-13 17:49:38 -0700

    test: add unit test for Kaui::Account
    Note: we had to hack around the fixtures mechanism as we don't rely on a
    database. Fixtures are used as json hashes, to mimic the payload from
    killbill server.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,3 +8,35 @@ Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+
+class ActiveRecord::Fixtures
+  # Monkey-patch the create_fixtures method not to rely on a database
+  def self.create_fixtures(fixtures_directory, table_names, class_names = {})
+    table_names = [table_names].flatten.map { |n| n.to_s }
+    table_names.each { |n|
+      class_names[n.tr('/', '_').to_sym] = n.classify if n.include?('/')
+    }
+
+    unless table_names.empty?
+      fixtures_map = {}
+      table_names.map do |path|
+        table_name = path.tr '/', '_'
+        fixtures_map[path] = ActiveRecord::Fixtures.new(nil,
+                                                        table_name,
+                                                        class_names[table_name.to_sym] || table_name.classify,
+                                                        ::File.join(fixtures_directory, path))
+      end
+
+      all_loaded_fixtures.update(fixtures_map)
+      cache_fixtures(nil, fixtures_map)
+    end
+    cached_fixtures(nil, table_names)
+  end
+end
+
+class ActiveRecord::Fixture
+  def find
+    @fixture
+  end
+end
