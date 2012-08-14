8d4868d2209e281ad27c8de5eb3c4702758186b3
Pierre-Alexandre Meyer <pierre@ning.com>
2012-08-13 20:51:51 -0700

    config: introduce locales
    Update the dummy app to be able to test internationalization via query
    parameters.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
