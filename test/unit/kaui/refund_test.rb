2c4f17af4f440b668864ce6db0940659622b96d0
Pierre-Alexandre Meyer <pierre@ning.com>
2012-08-15 18:03:45 -0700

    refund: various fixes around Refund model
    * Remove unnecessary constructor
    * Rename requested_dt and effective_dt to *_date for consistency
    * Fix effective date in timeline (typo)
    * Add unit tests and fixtures
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

---
+++ test/unit/kaui/refund_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class Kaui::RefundTest < ActiveSupport::TestCase
+  fixtures :refunds
+
+  test "can serialize from json" do
+    as_json = refunds(:refund_for_pierre)
+    refund = Kaui::Refund.new(as_json)
+    
+    assert_equal as_json["refund_id"], refund.refund_id
+    assert_equal as_json["paymentId"], refund.payment_id
+    assert_equal as_json["refundAmount"], refund.refund_amount
+    assert_equal as_json["adjusted"], refund.adjusted
+    assert_equal as_json["requestedDate"], refund.requested_date
+    assert_equal as_json["effectiveDate"], refund.effective_date
+    assert_equal as_json["adjustments"], refund.adjustments
+  end
+end
