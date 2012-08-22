49467f67f79d5ea266202d55ed5e369bbc3e09ad
Pierre-Alexandre Meyer <pierre@ning.com>
2012-08-21 19:35:51 -0700

    tag_definition: add new fields added in TagDefinitionJson
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- test/unit/kaui/tag_definition_test.rb
+++ test/unit/kaui/tag_definition_test.rb
@@ -4,20 +4,31 @@ class Kaui::TagDefinitionTest < ActiveSupport::TestCase
   fixtures :tag_definitions
 
   test "can serialize from json" do
+    # Test user tag
     as_json = tag_definitions(:payment_plan)
     tag_definition = Kaui::TagDefinition.new(as_json)
 
     assert_equal as_json["id"], tag_definition.id
     assert_equal as_json["name"], tag_definition.name
     assert_equal as_json["description"], tag_definition.description
+    assert_equal as_json["applicableObjectTypes"], tag_definition.applicable_object_types
     assert !tag_definition.is_system_tag?
 
+    # Test system tag
     as_json = tag_definitions(:auto_pay_off)
     tag_definition = Kaui::TagDefinition.new(as_json)
 
     assert_equal as_json["id"], tag_definition.id
     assert_equal as_json["name"], tag_definition.name
     assert_equal as_json["description"], tag_definition.description
+    assert_equal as_json["applicableObjectTypes"], tag_definition.applicable_object_types
     assert tag_definition.is_system_tag?
+  end
+
+  test "can find all per object type" do
+    assert_equal 3, Kaui::TagDefinition.all.size
+    assert_equal 2, Kaui::TagDefinition.all_for_account.size
+    assert_equal 1, Kaui::TagDefinition.all_for_invoice.size
+    assert_equal 1, Kaui::TagDefinition.all_for_tag_definition.size
   end
 end
