7a0ecb7ea658cfc12606e7199b5b716ec552aa02
Pierre-Alexandre Meyer <pierre@mouraf.org>
2013-08-18 10:52:47 -0400

    lib: get rid of error_helper
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- app/controllers/kaui/engine_controller.rb
+++ app/controllers/kaui/engine_controller.rb
@@ -1,9 +1,5 @@
-require 'kaui/error_helper'
-
 class Kaui::EngineController < ApplicationController
   before_filter :authenticate_user!
-
-  include Kaui::ErrorHelper
 
   layout :get_layout
 
@@ -28,6 +24,32 @@ class Kaui::EngineController < ApplicationController
   end
 
   protected
+
+  def as_string(e)
+    if e.is_a?(RestClient::Exception)
+      "#{e.message}, server response: #{as_string_from_response(e.response)}".split(/\n/).take(5).join("\n")
+    elsif e.is_a?(KillBillClient::API::ResponseError)
+      "Error #{e.response.code}: #{as_string_from_response(e.response.body)}"
+    else
+      e.message
+    end
+  end
+
+  def as_string_from_response(response)
+    error_message = response
+    begin
+      # BillingExceptionJson?
+      error_message = JSON.parse response
+    rescue => e
+    end
+
+    if error_message.respond_to? :[] and error_message['message'].present?
+      # Likely BillingExceptionJson
+      error_message = error_message['message']
+    end
+    # Limit the error size to avoid ActionDispatch::Cookies::CookieOverflow
+    error_message[0..1000]
+  end
 
   def get_layout
     layout ||= Kaui.config[:layout]
