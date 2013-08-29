78989d8a4a54aeb2a630c8440179cf55c45370e9
stephane brossier <stephane@ning.com>
2013-08-29 15:36:32 -0700

    Fix issue with dates in account timeline where day appeared one day ahead

--- app/helpers/kaui/date_helper.rb
+++ app/helpers/kaui/date_helper.rb
@@ -1,6 +1,17 @@
 module Kaui
   module DateHelper
+
+    LOCAL_DATE_RE = /^\d+-\d+-\d+$/
+
     def format_date(date, timezone="Pacific Time (US & Canada)")
+
+      # Double check this is not null
+      return nil if date.nil?
+
+      # If this is a local date we assume this is already in the account timezone, and so there is nothing to do
+      return date.to_s if LOCAL_DATE_RE.match(date.to_s)
+
+      # If not, convert into account timezone and return the date part only
       parsed_date = DateTime.parse(date.to_s).in_time_zone(timezone)
       parsed_date.to_s(:date_only)
     end
