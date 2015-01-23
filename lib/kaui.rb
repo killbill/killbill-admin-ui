35ba4d30f1d706f0922432b6264beaab191fff76
stephane brossier <sbrossier@groupon.com>
2015-01-23 15:55:22 -0800

    Add mattr_accessor for self.destroy_user_session_path

--- lib/kaui.rb
+++ lib/kaui.rb
@@ -12,6 +12,7 @@ module Kaui
   mattr_accessor :tenant_home_path
   mattr_accessor :select_tenant
   mattr_accessor :new_user_session_path
+  mattr_accessor :destroy_user_session_path
 
   mattr_accessor :bundle_key_display_string
   mattr_accessor :creditcard_plugin_name
@@ -24,7 +25,7 @@ module Kaui
   self.tenant_home_path = lambda { Kaui::Engine.routes.url_helpers.tenants_path }
   self.select_tenant = lambda { Kaui::Engine.routes.url_helpers.select_tenant_path }
   self.new_user_session_path = lambda { Kaui::Engine.routes.url_helpers.new_user_session_path }
-
+  self.destroy_user_session_path = lambda { Kaui::Engine.routes.url_helpers.destroy_user_session_path }
 
   self.bundle_key_display_string =  lambda {|bundle_key| bundle_key }
   self.creditcard_plugin_name =  lambda { '__EXTERNAL_PAYMENT__' }
@@ -66,7 +67,6 @@ module Kaui
 
 
   def self.config(&block)
-    # TODO
     {
       :layout => layout || 'kaui/layouts/kaui_application',
     }
