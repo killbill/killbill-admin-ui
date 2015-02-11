5898fc945bb7a1797e3e011d941dbb75acc2b9de
stephane brossier <sbrossier@groupon.com>
2015-02-11 15:12:46 -0800

    Fix Permission.java (killbill-api) to have clean model and fix'up CanCan to make the correct translation to KAUI models

--- app/models/kaui/ability.rb
+++ app/models/kaui/ability.rb
@@ -7,7 +7,7 @@ module Kaui
       user.permissions.each do |permission|
         # permission is something like invoice:item_adjust or payment:refund
         # We rely on a naming convention where the left part refers to a Kaui model
-        model, action = permission.split(':')
+        model, action = permission_to_model_action(permission)
         if model == '*' and action == '*'
           # All permissions!
           can :manage, :all
@@ -21,6 +21,22 @@ module Kaui
         end
       end
     rescue KillBillClient::API::Unauthorized => e
+    end
+
+    def permission_to_model_action(permission)
+      #
+      # Permissions are defined in Kill Kill apis (https://github.com/killbill/killbill-api/blob/master/src/main/java/org/killbill/billing/security/Permission.java)
+      # and they look something like 'invoice:item_adjust' or 'payment:refund', where the first part is the Kill Bill module and the second the action.
+      #
+      # For most of those the Kill Bill module maps to the Kaui model, but for a few, the naming convention breaks, so in order to keep the API clean, we do the fix up
+      # in KAUI itself:
+      #
+      to_be_model, action = permission.split(':')
+      # Currently the only actions implemented for overdue and catalog (upload_config) are those implemented at the tenant level:
+      if to_be_model == 'tenant' || 'overdue' || 'catalog'
+        to_be_model = 'admin_tenant'
+      end
+      [to_be_model, action]
     end
   end
 end
