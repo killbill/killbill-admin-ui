killbill/killbill-admin-ui

commit 315733d4b4f13afe2d9e2cab63e5360d5b213b14

Fixes #37

app/controllers/kaui/admin_tenants_controller.rb
@@ -58,6 +58,11 @@ module Kaui
 
     def show
       @tenant = Kaui::Tenant.find(params[:id])
+      user = current_user
+      if @tenant.kaui_allowed_users.index { |e| e.kb_username == user.kb_username}.nil?
+        flash[:error] = "Does not have permissions to see tenant id #{params[:id]}"
+        redirect_to admin_tenants_path and return
+      end
       render
     end
 

app/views/kaui/admin_tenants/show.html.erb
@@ -1,13 +1,13 @@
-<% if can? :view, Kaui::AdminTenant %>
-    <dl class="dl-horizontal">
-      <dt>Name:</dt>
-      <dd><%= @tenant.name %>&nbsp;</dd>
-      <dt>Tenant ID:</dt>
-      <dd><%= @tenant.kb_tenant_id %>&nbsp;</dd>
-      <dt>API Key:</dt>
-      <dd><%= @tenant.api_key %>&nbsp;</dd>
-    </dl>
+<dl class="dl-horizontal">
+  <dt>Name:</dt>
+  <dd><%= @tenant.name %>&nbsp;</dd>
+  <dt>Tenant ID:</dt>
+  <dd><%= @tenant.kb_tenant_id %>&nbsp;</dd>
+  <dt>API Key:</dt>
+  <dd><%= @tenant.api_key %>&nbsp;</dd>
+</dl>
 
+<% if can? :view, Kaui::AdminTenant %>
     <% unless @tenant.kaui_allowed_users.empty? %>
         <h3>Allowed Users:</h3>
         <table id="allowed-users-for-tenant-table" class="table table-condensed">

commit 42ad33743dd8e676deed4d870d4db91b26745836

Fix regression introduced by 315733d4b4f13afe2d9e2cab63e5360d5b213b14 and add new test to verify correct redirect and error when user does not have access to tenant

test/functional/kaui/admin_tenants_controller_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 
 module Kaui
   class AdminTenantsControllerTest < Kaui::FunctionalTestHelper
-
     test "should get new" do
       post :new
       assert_response :success
@@ -14,21 +13,40 @@ module Kaui
       assert_response 302
     end
 
-    test "should get show" do
+
+    test "should get index" do
+      get :index
+      assert_response :success
+    end
+
+    test "should get show with allowed user" do
       tenant = Kaui::Tenant.new
       tenant.name = 'foo'
       tenant.api_key = 'api_key'
       tenant.api_secret = 'api_secret'
       tenant.kb_tenant_id = 'kb_tenant_id'
       tenant.save!
+
+      # Add an allowed user that will verify that we can only
+      au = Kaui::AllowedUser.find_by_kb_username('admin')
+      au.kaui_tenants << tenant
+
       get :show, :id => tenant.id
       assert_response :success
     end
 
+    test "should get show with NO allowed user" do
+      tenant = Kaui::Tenant.new
+      tenant.name = 'foo'
+      tenant.api_key = 'api_key'
+      tenant.api_secret = 'api_secret'
+      tenant.kb_tenant_id = 'kb_tenant_id'
+      tenant.save!
 
-    test "should get index" do
-      get :index
-      assert_response :success
+      get :show, :id => tenant.id
+      assert_response 302
+      # Clear it before we call check_no_flash_error
+      flash[:error] = nil
     end
 
   end