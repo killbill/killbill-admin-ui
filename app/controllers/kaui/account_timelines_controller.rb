killbill/killbill-admin-ui

commit 7fada302e2621447d3d9ccb7a14b160d3dd896e5

Fixes #44 (BillingTimeline is incredibly slow)

app/controllers/kaui/account_timelines_controller.rb
@@ -32,13 +32,7 @@ class Kaui::AccountTimelinesController < Kaui::EngineController
     end
 
     # Lookup all invoices
-    @invoices_by_id = {}
-    @timeline.payments.each do |payment|
-      load_invoice_id_for_timeline(payment.target_invoice_id)
-    end
-    @timeline.invoices.each do |invoice|
-      load_invoice_id_for_timeline(invoice.invoice_id)
-    end
+    load_invoices_for_timeline
 
     if params.has_key?(:external_key)
       @selected_bundle = @bundle_names[params[:external_key]]
@@ -49,6 +43,14 @@ class Kaui::AccountTimelinesController < Kaui::EngineController
 
   def load_bundle_name_for_timeline(bundle_key)
     @bundle_names[bundle_key] ||= Kaui.bundle_key_display_string.call(bundle_key)
+  end
+
+  def load_invoices_for_timeline
+    all_invoices = @account.invoices(true, options_for_klient)
+    return {} if all_invoices.nil? || all_invoices.empty?
+
+    # Convert into Kaui::Invoice to benefit from additional methods xxx_to_money
+    @invoices_by_id = all_invoices.inject({}) {|hsh, invoice| hsh[invoice.invoice_id] = Kaui::Invoice.new(invoice); hsh}
   end
 
   def load_invoice_id_for_timeline(invoice_id)

app/models/kaui/invoice.rb
@@ -1,5 +1,23 @@
 class Kaui::Invoice < KillBillClient::Model::Invoice
 
+  def initialize(raw_invoice)
+    # There is probably a meta-programming trick to avoid writing that copy ctor by hand...
+    @amount = raw_invoice.amount
+    @currency = raw_invoice.currency
+    @credit_adj = raw_invoice.credit_adj
+    @refund_adj = raw_invoice.refund_adj
+    @invoice_id = raw_invoice.invoice_id
+    @invoice_date = raw_invoice.invoice_date
+    @target_date = raw_invoice.target_date
+    @invoice_number = raw_invoice.invoice_number
+    @balance = raw_invoice.balance
+    @account_id = raw_invoice.account_id
+    @external_bundle_keys = raw_invoice.external_bundle_keys
+    @credits = raw_invoice.credits
+    @items = raw_invoice.items
+    @audit_logs = raw_invoice.audit_logs
+  end
+
   [:amount, :balance, :credits].each do |type|
     define_method "#{type}_to_money" do
       Kaui::Base.to_money(send(type), currency)

commit 6906eb594f62c212e9a23e6724c297e8939c4bd7

Fix issues introduced by previous commit (7fada302e2621447d3d9ccb7a14b160d3dd896e5) and making the tests fail.

app/controllers/kaui/account_timelines_controller.rb
@@ -50,16 +50,6 @@ class Kaui::AccountTimelinesController < Kaui::EngineController
     return {} if all_invoices.nil? || all_invoices.empty?
 
     # Convert into Kaui::Invoice to benefit from additional methods xxx_to_money
-    @invoices_by_id = all_invoices.inject({}) {|hsh, invoice| hsh[invoice.invoice_id] = Kaui::Invoice.new(invoice); hsh}
-  end
-
-  def load_invoice_id_for_timeline(invoice_id)
-    return if invoice_id.blank? or @invoices_by_id.has_key?(invoice_id)
-
-    begin
-      @invoices_by_id[invoice_id] = Kaui::Invoice.find_by_id_or_number(invoice_id, true, 'NONE', options_for_klient)
-    rescue => e
-      flash.now[:error] = "Could not get invoice information for the timeline #{@account_id}: #{as_string(e)}"
-    end
+    @invoices_by_id = all_invoices.inject({}) {|hsh, invoice| hsh[invoice.invoice_id] = Kaui::Invoice.build_from_raw_invoice(invoice); hsh}
   end
 end

app/models/kaui/invoice.rb
@@ -1,22 +1,25 @@
 class Kaui::Invoice < KillBillClient::Model::Invoice
 
-  def initialize(raw_invoice)
+  def self.build_from_raw_invoice(raw_invoice)
     # There is probably a meta-programming trick to avoid writing that copy ctor by hand...
-    @amount = raw_invoice.amount
-    @currency = raw_invoice.currency
-    @credit_adj = raw_invoice.credit_adj
-    @refund_adj = raw_invoice.refund_adj
-    @invoice_id = raw_invoice.invoice_id
-    @invoice_date = raw_invoice.invoice_date
-    @target_date = raw_invoice.target_date
-    @invoice_number = raw_invoice.invoice_number
-    @balance = raw_invoice.balance
-    @account_id = raw_invoice.account_id
-    @external_bundle_keys = raw_invoice.external_bundle_keys
-    @credits = raw_invoice.credits
-    @items = raw_invoice.items
-    @audit_logs = raw_invoice.audit_logs
+    result = Kaui::Invoice.new
+    result.amount = raw_invoice.amount
+    result.currency = raw_invoice.currency
+    result.credit_adj = raw_invoice.credit_adj
+    result.refund_adj = raw_invoice.refund_adj
+    result.invoice_id = raw_invoice.invoice_id
+    result.invoice_date = raw_invoice.invoice_date
+    result.target_date = raw_invoice.target_date
+    result.invoice_number = raw_invoice.invoice_number
+    result.balance = raw_invoice.balance
+    result.account_id = raw_invoice.account_id
+    result.external_bundle_keys = raw_invoice.external_bundle_keys
+    result.credits = raw_invoice.credits
+    result.items = raw_invoice.items
+    result.audit_logs = raw_invoice.audit_logs
+    result
   end
+
 
   [:amount, :balance, :credits].each do |type|
     define_method "#{type}_to_money" do