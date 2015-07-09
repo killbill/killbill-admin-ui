34fb081c629095e96337ecb51d0cee5c14f7271c
James <jaimersonaraujo@gmail.com>
2015-07-09 01:01:45 -0300

    Change pagination type

--- app/assets/javascripts/kaui/datatables-tweaks.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('.dataTables_paginate').hide();
-});
-
-$(document).on('init.dt', function(){
-  $('.dataTables_paginate').find('ul').addClass('pagination');
-  $('.dataTables_paginate').show();
-});
