8ae88f9b019846e09aed7214a3d6089fb27c9223
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-08-29 08:25:15 -0700

    gemspec: update killbill-client to 0.17
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,8 @@ require 'rails/test_help'
 
 Rails.backtrace_cleaner.remove_silencers!
 
+require 'multi_json'
+
 # Include helpers
 require 'killbill_test_helper'
 require 'functional/kaui/functional_test_helper_nosetup'
