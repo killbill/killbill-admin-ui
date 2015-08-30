dde0489cce7ad3134cb17ef00aea6859fdf13628
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-08-30 11:52:54 -0700

    accounts: better errors handling
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- app/controllers/kaui/accounts_controller.rb
+++ app/controllers/kaui/accounts_controller.rb
@@ -6,16 +6,16 @@ class Kaui::AccountsController < Kaui::EngineController
 
   def pagination
     search_key = params[:sSearch]
-    offset     = params[:iDisplayStart] || 0
-    limit      = params[:iDisplayLength] || 10
+    offset = params[:iDisplayStart] || 0
+    limit = params[:iDisplayLength] || 10
 
     accounts = Kaui::Account.list_or_search(search_key, offset, limit, options_for_klient)
 
     json = {
-        :sEcho                => params[:sEcho],
-        :iTotalRecords        => accounts.pagination_max_nb_records,
+        :sEcho => params[:sEcho],
+        :iTotalRecords => accounts.pagination_max_nb_records,
         :iTotalDisplayRecords => accounts.pagination_total_nb_records,
-        :aaData               => []
+        :aaData => []
     }
 
     accounts.each do |account|
@@ -39,10 +39,10 @@ class Kaui::AccountsController < Kaui::EngineController
   end
 
   def create
-    @account                          = Kaui::Account.new(params[:account].delete_if { |key, value| value.blank? })
+    @account = Kaui::Account.new(params.require(:account).delete_if { |key, value| value.blank? })
 
     # Transform "1" into boolean
-    @account.is_migrated              = @account.is_migrated == '1'
+    @account.is_migrated = @account.is_migrated == '1'
     @account.is_notified_for_invoices = @account.is_notified_for_invoices == '1'
 
     begin
@@ -55,58 +55,38 @@ class Kaui::AccountsController < Kaui::EngineController
   end
 
   def show
-    begin
-      @account       = Kaui::Account::find_by_id_or_key(params[:id], true, true, options_for_klient)
-      @overdue_state = @account.overdue(options_for_klient)
-      @tags          = @account.tags(false, 'NONE', options_for_klient).sort { |tag_a, tag_b| tag_a <=> tag_b }
+    @account = Kaui::Account::find_by_id_or_key(params.require(:id), true, true, options_for_klient)
+    @overdue_state = @account.overdue(options_for_klient)
+    @tags = @account.tags(false, 'NONE', options_for_klient).sort { |tag_a, tag_b| tag_a <=> tag_b }
 
-      @account_emails  = Kaui::AccountEmail.find_all_sorted_by_account_id(@account.account_id, 'NONE', options_for_klient)
-      @payment_methods = Kaui::PaymentMethod.find_non_external_by_account_id(@account.account_id, true, options_for_klient)
+    @account_emails = Kaui::AccountEmail.find_all_sorted_by_account_id(@account.account_id, 'NONE', options_for_klient)
+    @payment_methods = Kaui::PaymentMethod.find_non_external_by_account_id(@account.account_id, true, options_for_klient)
 
-      render_with_account_navbar
-    rescue => e
-      flash.now[:error] = "Error while retrieving account information: #{as_string(e)}"
-      render :action => :index and return
-    end
+    render_with_account_navbar
   end
 
   def set_default_payment_method
-    account_id        = params[:id]
-    payment_method_id = params[:payment_method_id]
+    account_id = params.require(:id)
+    payment_method_id = params.require(:payment_method_id)
 
-    begin
-      Kaui::PaymentMethod.set_default(payment_method_id, account_id, current_user.kb_username, params[:reason], params[:comment], options_for_klient)
-      flash[:notice] = "Successfully set #{payment_method_id} as default"
-    rescue => e
-      flash[:error] = "Error while setting payment method #{payment_method_id} as default: #{as_string(e)}"
-    end
+    Kaui::PaymentMethod.set_default(payment_method_id, account_id, current_user.kb_username, params[:reason], params[:comment], options_for_klient)
 
-    redirect_to account_path(account_id)
+    redirect_to account_path(account_id), :notice => "Successfully set #{payment_method_id} as default"
   end
 
   def toggle_email_notifications
-    account = Kaui::Account.new(:account_id => params[:id], :is_notified_for_invoices => params[:is_notified])
+    account = Kaui::Account.new(:account_id => params.require(:id), :is_notified_for_invoices => params[:is_notified] == 'true')
 
-    begin
-      account.update_email_notifications(current_user.kb_username, params[:reason], params[:comment], options_for_klient)
-      flash[:notice] = 'Email preferences updated'
-    rescue => e
-      flash[:error] = "Error while setting email notifications: #{as_string(e)}"
-    end
+    account.update_email_notifications(current_user.kb_username, params[:reason], params[:comment], options_for_klient)
 
-    redirect_to account_path(account.account_id)
+    redirect_to account_path(account.account_id), :notice => 'Email preferences updated'
   end
 
   def pay_all_invoices
-    payment = Kaui::InvoicePayment.new(:account_id => params[:id])
+    payment = Kaui::InvoicePayment.new(:account_id => params.require(:id))
 
-    begin
-      payment.bulk_create(params[:is_external_payment], current_user.kb_username, params[:reason], params[:comment], options_for_klient)
-      flash[:notice] = 'Successfully triggered a payment for all unpaid invoices'
-    rescue => e
-      flash[:error] = "Error while triggering payments: #{as_string(e)}"
-    end
+    payment.bulk_create(params[:is_external_payment] == 'true', current_user.kb_username, params[:reason], params[:comment], options_for_klient)
 
-    redirect_to account_path(payment.account_id)
+    redirect_to account_path(payment.account_id), :notice => 'Successfully triggered a payment for all unpaid invoices'
   end
 end
--- test/functional/kaui/accounts_controller_test.rb
+++ test/functional/kaui/accounts_controller_test.rb
@@ -19,6 +19,13 @@ class Kaui::AccountsControllerTest < Kaui::FunctionalTestHelper
     verify_pagination_results!
   end
 
+  test 'should handle Kill Bill errors when showing account details' do
+    account_id = SecureRandom.uuid.to_s
+    get :show, :id => account_id
+    assert_redirected_to home_path
+    assert_equal "Error while communicating with the Kill Bill server: Error 404: Account does not exist for id #{account_id}", flash[:error]
+  end
+
   test 'should find account by id' do
     get :show, :id => @account.account_id
     assert_response 200
@@ -28,6 +35,20 @@ class Kaui::AccountsControllerTest < Kaui::FunctionalTestHelper
     assert_not_nil assigns(:payment_methods)
   end
 
+  test 'should handle Kill Bill errors when creating account' do
+    post :create
+    assert_redirected_to home_path
+    assert_equal 'Required parameter missing: account', flash[:error]
+
+    external_key = SecureRandom.uuid.to_s
+    post :create, :account => {:external_key => external_key}
+    assert_redirected_to account_path(assigns(:account).account_id)
+
+    post :create, :account => {:external_key => external_key}
+    assert_template :new
+    assert_equal "Error while creating account: Error 409: Account already exists for key #{external_key}", flash[:error]
+  end
+
   test 'should create account' do
     get :new
     assert_response 200
@@ -35,12 +56,12 @@ class Kaui::AccountsControllerTest < Kaui::FunctionalTestHelper
 
     post :create,
          :account => {
-             :name         => SecureRandom.uuid.to_s,
+             :name => SecureRandom.uuid.to_s,
              :external_key => SecureRandom.uuid.to_s,
-             :email        => SecureRandom.uuid.to_s + '@example.com',
-             :time_zone    => '-06:00',
-             :country      => 'AR',
-             :is_migrated  => '1'
+             :email => SecureRandom.uuid.to_s + '@example.com',
+             :time_zone => '-06:00',
+             :country => 'AR',
+             :is_migrated => '1'
          }
     assert_redirected_to account_path(assigns(:account).account_id)
     assert_equal 'Account was successfully created', flash[:notice]
@@ -51,14 +72,41 @@ class Kaui::AccountsControllerTest < Kaui::FunctionalTestHelper
     assert !assigns(:account).is_notified_for_invoices
   end
 
+  test 'should be redirected if no payment_method_id is specified when setting default payment method' do
+    put :set_default_payment_method, :id => @account.account_id
+    assert_redirected_to home_path
+    assert_equal 'Required parameter missing: payment_method_id', flash[:error]
+  end
+
+  test 'should handle Kill Bill errors when setting default payment method' do
+    account_id = SecureRandom.uuid.to_s
+    put :set_default_payment_method, :id => account_id, :payment_method_id => @payment_method.payment_method_id
+    assert_redirected_to home_path
+    assert_equal "Error while communicating with the Kill Bill server: Error 404: Account does not exist for id #{account_id}", flash[:error]
+  end
+
   test 'should set default payment method' do
     put :set_default_payment_method, :id => @account.account_id, :payment_method_id => @payment_method.payment_method_id
     assert_response 302
   end
 
+  test 'should handle Kill Bill errors when toggling email notifications' do
+    account_id = SecureRandom.uuid.to_s
+    put :toggle_email_notifications, :id => account_id, :is_notified => true
+    assert_redirected_to home_path
+    assert_equal "Error while communicating with the Kill Bill server: Error 404: Account does not exist for id #{account_id}", flash[:error]
+  end
+
   test 'should toggle email notifications' do
     put :toggle_email_notifications, :id => @account.account_id, :is_notified => true
     assert_response 302
+  end
+
+  test 'should handle Kill Bill errors when paying all invoices' do
+    account_id = SecureRandom.uuid.to_s
+    post :pay_all_invoices, :id => account_id
+    assert_redirected_to home_path
+    assert_equal "Error while communicating with the Kill Bill server: Error 404: Account does not exist for id #{account_id}", flash[:error]
   end
 
   test 'should pay all invoices' do
