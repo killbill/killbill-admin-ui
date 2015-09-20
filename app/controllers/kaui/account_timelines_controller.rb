4f8e9f1f5aa39be2676fccb4b261b73f4c035861
stephane brossier <sbrossier@groupon.com>
2015-09-19 19:56:28 -0700

    Remove unused api calls for account timeline (only one call (timeline) will suffice)

--- app/controllers/kaui/account_timelines_controller.rb
+++ app/controllers/kaui/account_timelines_controller.rb
@@ -1,21 +1,21 @@
 class Kaui::AccountTimelinesController < Kaui::EngineController
 
   def show
-    @account = Kaui::Account.find_by_id(params.require(:account_id), false, false, options_for_klient)
-
-    fetch_timeline = lambda { @timeline = Kaui::AccountTimeline.find_by_account_id(@account.account_id, 'FULL', options_for_klient) }
-    fetch_invoices = lambda { load_invoices_for_timeline }
-
-    run_in_parallel fetch_timeline, fetch_invoices
+    timeline = Kaui::AccountTimeline.find_by_account_id(params.require(:account_id), 'FULL', options_for_klient)
+    @account = timeline.account
+    @bundles = timeline.bundles
+    @invoices = timeline.invoices
+    @payments = timeline.payments
+    extract_invoices_by_id(@invoices)
 
     # Lookup all bundle names
     @bundle_names = {}
     @bundle_names_by_invoice_id = {}
     @bundle_keys_by_invoice_id = {}
-    @timeline.bundles.each do |bundle|
+    @bundles.each do |bundle|
       load_bundle_name_for_timeline(bundle.external_key)
     end
-    @timeline.invoices.each do |invoice|
+    @invoices.each do |invoice|
       @bundle_names_by_invoice_id[invoice.invoice_id] = Set.new
       @bundle_keys_by_invoice_id[invoice.invoice_id] = Set.new
       (invoice.bundle_keys || '').split(',').each do |bundle_key|
@@ -36,8 +36,7 @@ class Kaui::AccountTimelinesController < Kaui::EngineController
     @bundle_names[bundle_key] ||= Kaui.bundle_key_display_string.call(bundle_key)
   end
 
-  def load_invoices_for_timeline
-    all_invoices = @account.invoices(true, options_for_klient)
+  def extract_invoices_by_id(all_invoices)
     return {} if all_invoices.nil? || all_invoices.empty?
 
     # Convert into Kaui::Invoice to benefit from additional methods xxx_to_money
