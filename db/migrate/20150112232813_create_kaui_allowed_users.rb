2c5116892c3ac328dc08c7983c3e9ae2461126bf
stephane brossier <sbrossier@groupon.com>
2015-09-21 12:32:32 -0700

    Add migration file (travis seems to be broken)

---
+++ db/migrate/20150112232813_create_kaui_allowed_users.rb
@@ -0,0 +1,19 @@
+class CreateKauiAllowedUsers < ActiveRecord::Migration
+  def change
+    create_table :kaui_allowed_users do |t|
+      t.string :kb_username
+      t.string :description
+      t.timestamps
+    end
+
+    add_index :kaui_allowed_users, [:kb_username], :unique => true
+
+    create_table :kaui_allowed_user_tenants do |t|
+      t.belongs_to :kaui_allowed_user
+      t.belongs_to :kaui_tenant
+      t.timestamps null: false
+    end
+
+    add_index :kaui_allowed_user_tenants, [:kaui_allowed_user_id, :kaui_tenant_id], :unique => true, :name => 'kaui_allowed_user_tenants_uniq'
+  end
+end
