778f3f1cca3b05fa52c31a646972937db5a98af2
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-10-22 15:28:58 -0700

    engine_controller: trivial cleanups
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- app/controllers/kaui/engine_controller.rb
+++ app/controllers/kaui/engine_controller.rb
@@ -26,9 +26,9 @@ class Kaui::EngineController < ApplicationController
 
   def check_for_redirect_to_tenant_screen
     if !Kaui.is_user_assigned_valid_tenant?(current_user, session)
-      flash[:error] = "No tenants configured for users AND KillBillClient.api_key, KillBillClient.api_secret have not been set"
+      flash[:error] = 'No tenants configured for users AND KillBillClient.api_key, KillBillClient.api_secret have not been set'
       session[:kb_tenant_id] = nil
-      redirect_to Kaui.tenant_home_path.call and return
+      redirect_to Kaui.tenant_home_path.call
     end
   end
 
