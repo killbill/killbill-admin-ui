51bdce249acc732da504751c55d180ba304032c8
Joel Batista <jbatista@wovenware.com>
2017-10-24 11:43:19 -0400

    Issue #65
    KAUI functionality isNotifiedForInvoice needs to be work with email plugin #65

--- app/helpers/kaui/account_helper.rb
+++ app/helpers/kaui/account_helper.rb
@@ -5,5 +5,9 @@ module Kaui
       return nil if @account.nil?
       @account.name.presence || @account.email.presence || truncate_uuid(@account.external_key)
     end
+
+    def email_notifications_plugin_available?
+      Kenui::EmailNotificationService.email_notification_plugin_available?(Kaui.current_tenant_user_options(current_user, session)).first
+    end
   end
 end
