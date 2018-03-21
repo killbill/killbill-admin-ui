cda26388adfee85685648dcf355d4b3597684e09
Pierre-Alexandre Meyer <pierre@mouraf.org>
2018-03-21 02:56:32 -0700

    migrations: detect if tables already exist
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- db/migrate/20150109214021_create_kaui_tenants.rb
+++ db/migrate/20150109214021_create_kaui_tenants.rb
@@ -1,12 +1,14 @@
 class CreateKauiTenants < ActiveRecord::Migration[5.0]
   def change
-    create_table :kaui_tenants do |t|
-      t.string :name
-      t.string :kb_tenant_id
-      t.string :api_key
-      t.string :encrypted_api_secret
+    unless table_exists?(:kaui_tenants)
+      create_table :kaui_tenants do |t|
+        t.string :name
+        t.string :kb_tenant_id
+        t.string :api_key
+        t.string :encrypted_api_secret
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
