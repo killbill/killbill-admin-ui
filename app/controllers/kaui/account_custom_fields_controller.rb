2720e9826e025d1f2855b3e937302633f5c31d36
Joel Batista <jbatista@wovenware.com>
2018-04-13 16:43:41 -0400

    Issue#170 - CUSTOM FIELDS list into the account page

---
+++ app/controllers/kaui/account_custom_fields_controller.rb
@@ -0,0 +1,24 @@
+class Kaui::AccountCustomFieldsController < Kaui::EngineController
+
+  def index
+    cached_options_for_klient = options_for_klient
+    account = Kaui::Account::find_by_id_or_key(params.require(:account_id), true, true, cached_options_for_klient)
+    custom_fields = account.all_custom_fields(nil, 'NONE', cached_options_for_klient)
+
+
+    formatter = lambda do |custom_field|
+      url_for_object = view_context.url_for_object(custom_field.object_id, custom_field.object_type)
+      [
+          url_for_object ? view_context.link_to(custom_field.object_id, url_for_object) : custom_field.object_id,
+          custom_field.object_type,
+          custom_field.name,
+          custom_field.value
+      ]
+    end
+    @custom_fields_json = []
+    custom_fields.each { |page| @custom_fields_json << formatter.call(page) }
+
+    @custom_fields_json = @custom_fields_json.to_json
+  end
+
+end
---
+++ test/functional/kaui/account_custom_fields_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class Kaui::AccountCustomFieldsControllerTest < Kaui::FunctionalTestHelper
+
+  test 'should list all account custom fields' do
+    new_account = create_account(@tenant)
+
+    # set a custom field
+    created_custom_field = Kaui::CustomField.new
+    created_custom_field.object_id = new_account.account_id
+    created_custom_field.object_type = 'ACCOUNT'
+    created_custom_field.name = 'test'
+    created_custom_field.value = 'value'
+
+    new_account.add_custom_field(created_custom_field, 'Kaui test', nil, nil, build_options(@tenant, USERNAME, PASSWORD))
+
+
+    # get custom field list
+    get :index, :account_id => new_account.account_id
+    assert_response :success
+    custom_fields_from_response = get_value_from_input_field('custom-fields').gsub!('&quot;','"');
+    assert_not_nil custom_fields_from_response
+    custom_fields = JSON.parse(custom_fields_from_response)
+    assert_equal 1, custom_fields.count
+    assert_equal 'ACCOUNT', custom_fields[0][1]
+    assert_equal 'test', custom_fields[0][2]
+    assert_equal 'value', custom_fields[0][3]
+  end
+end
