18f8f4fb25058ea6c2209a36bf5f5f5365428815
Pierre-Alexandre Meyer <pierre@mouraf.org>
2018-10-01 10:18:37 +0100

    test: remove incorrect test
    None of these control tags apply to bundles.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- test/functional/kaui/bundle_tags_controller_test.rb
+++ test/functional/kaui/bundle_tags_controller_test.rb
@@ -20,15 +20,4 @@ class Kaui::BundleTagsControllerTest < Kaui::FunctionalTestHelper
     assert_not_nil assigns(:tag_names)
     assert_not_nil assigns(:available_tags)
   end
-
-  test 'should update tags' do
-    post :update,
-         :account_id => @account.account_id,
-         :bundle_id => @bundle.bundle_id,
-         :'tag_00000000-0000-0000-0000-000000000001' => 'AUTO_PAY_OFF',
-         :'tag_00000000-0000-0000-0000-000000000005' => 'MANUAL_PAY',
-         :'tag_00000000-0000-0000-0000-000000000003' => 'OVERDUE_ENFORCEMENT_OFF'
-    assert_redirected_to account_bundles_path(@account.account_id)
-    assert_equal 'Bundle tags successfully set', flash[:notice]
-  end
 end
