290c396bed66891b4620ccb48e5ea2f285b9345e
stephane brossier <sbrossier@groupon.com>
2019-06-05 12:17:31 -0700

    Modify credit screen to be compatible with api change

--- app/controllers/kaui/credits_controller.rb
+++ app/controllers/kaui/credits_controller.rb
@@ -13,7 +13,7 @@ class Kaui::CreditsController < Kaui::EngineController
       currency = params[:currency] || 'USD'
     end
 
-    @credit = Kaui::Credit.new(:account_id => params.require(:account_id), :invoice_id => invoice_id, :credit_amount => amount, :currency => currency)
+    @credit = Kaui::Credit.new(:account_id => params.require(:account_id), :invoice_id => invoice_id, :amount => amount, :currency => currency)
   end
 
   def create
