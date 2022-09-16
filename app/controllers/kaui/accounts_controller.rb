e89adc37e8b4bc5d01915198269e8736190c5dbd
kpbacode <65641209+kpbacode@users.noreply.github.com>
2022-09-16 18:59:53 +0200

    Update accounts_controller.rb
    Clean awesome_print

--- app/controllers/kaui/accounts_controller.rb
+++ app/controllers/kaui/accounts_controller.rb
@@ -57,10 +57,7 @@ class Kaui::AccountsController < Kaui::EngineController
 
     @account = Kaui::Account.new(params.require(:account).delete_if { |key, value| value.blank? })
 
-    ap @account
-
     unless @account.valid?
-      # ap @account.errors.full_messages
       flash.now[:errors] = @account.errors.messages.values.flatten
       render :action => :new and return
     end
