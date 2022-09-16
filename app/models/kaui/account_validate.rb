61593dbb8a8b8b8b7c5313d390fcadee64aed76d
kpbacode <65641209+kpbacode@users.noreply.github.com>
2022-09-16 19:01:38 +0200

    Delete account_validate.rb

--- app/models/kaui/account_validate.rb
+++
@@ -1,5 +0,0 @@
-# class KauiPhoneValid
-#   include ActiveModel::Validations
-#   attr_accessor :phone
-#   validates :phone, format: { with: /\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/i, message: 'invalid phone'}
-# end
