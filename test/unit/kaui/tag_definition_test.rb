b5f647cfde8f70e3c36a9608b55313e6529f59ee
Gaurav Kumar Rana <138654702+grana-equinix@users.noreply.github.com>
2023-09-21 14:07:12 +0530

    Adds test case to check the list of tags in account information

--- test/unit/kaui/tag_definition_test.rb
+++ test/unit/kaui/tag_definition_test.rb
@@ -4,11 +4,21 @@ require 'test_helper'
 
 module Kaui
   class TagDefinitionTest < ActiveSupport::TestCase
+    include Kaui::KillbillTestHelper
+
     test 'can detect system tags' do
       1.upto(9).each do |i|
         assert Kaui::TagDefinition.new(id: "00000000-0000-0000-0000-00000000000#{i}").system_tag?
       end
       assert !Kaui::TagDefinition.new(id: SecureRandom.uuid).system_tag?
+    end
+
+    test 'can list all tags user and control' do
+      tenant = create_tenant
+      assert_not_nil(tenant)
+      options_for_klient = build_options(tenant)
+      tags = Kaui::TagDefinition.all_for_account(options_for_klient)
+      assert_equal 9, tags.count
     end
   end
 end
