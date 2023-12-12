9ba86b00bbddb3e006cd4bf5812ed2e412581c7e
Gaurav Kumar Rana <138654702+grana-equinix@users.noreply.github.com>
2023-12-12 12:24:27 +0530

    Method rename

--- lib/kaui.rb
+++ lib/kaui.rb
@@ -48,7 +48,7 @@ module Kaui
 
   mattr_accessor :disable_sign_up_link
   mattr_accessor :additional_headers_partial
-  mattr_accessor :customer_bulk_invoice_adjust_link
+  mattr_accessor :additional_invoice_links
 
   self.home_path = -> { Kaui::Engine.routes.url_helpers.home_path }
   self.tenant_home_path = -> { Kaui::Engine.routes.url_helpers.tenants_path }
@@ -105,7 +105,7 @@ module Kaui
 
   self.customer_invoice_link = ->(invoice, ctx) { ctx.link_to 'View customer invoice html', ctx.kaui_engine.show_html_invoice_path(invoice.invoice_id), class: 'btn', target: '_blank' }
 
-  self.customer_bulk_invoice_adjust_link = ->(invoice, ctx) {}
+  self.additional_invoice_links = ->(invoice, ctx) {}
 
   self.demo_mode = false
 
