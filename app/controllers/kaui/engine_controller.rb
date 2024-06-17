824cd41a11e5caf4b02b4611d70a9148c785dde5
Tung Le <tungleduy.xyz@gmail.com>
2024-06-17 23:50:10 +0700

    Moving handle exception to killbill-admin-ui-standalone

--- app/controllers/kaui/engine_controller.rb
+++ app/controllers/kaui/engine_controller.rb
@@ -1,6 +1,9 @@
+require_relative '../../../lib/kaui/error_handler'
+
 class Kaui::EngineController < ApplicationController
 
   include Kaui::EngineControllerUtil
+  include Kaui::ErrorHandler
 
   before_action :authenticate_user!, :check_for_redirect_to_tenant_screen, :populate_account_details
 
@@ -59,25 +62,6 @@ class Kaui::EngineController < ApplicationController
     end
   end
 
-  # Note! Order matters, StandardError needs to be first
-  rescue_from(StandardError) do |error|
-    flash[:error] = "Error: #{as_string(error)}"
-    try_to_redirect_to_account_path = !params[:controller].ends_with?('accounts')
-    perform_redirect_after_error try_to_redirect_to_account_path
-  end
-
-  rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
-    log_rescue_error parameter_missing_exception
-    flash[:error] = "Required parameter missing: #{parameter_missing_exception.param}"
-    perform_redirect_after_error
-  end
-
-  rescue_from(KillBillClient::API::ResponseError) do |killbill_exception|
-    flash[:error] = "Error while communicating with the Kill Bill server: #{as_string(killbill_exception)}"
-    try_to_redirect_to_account_path = !killbill_exception.is_a?(KillBillClient::API::Unauthorized) && !(killbill_exception.is_a?(KillBillClient::API::NotFound) && params[:controller].ends_with?('accounts'))
-    perform_redirect_after_error try_to_redirect_to_account_path
-  end
-
   private
 
   def current_tenant_user
@@ -94,14 +78,5 @@ class Kaui::EngineController < ApplicationController
       result[:api_secret] = user_tenant.api_secret
     end
     result
-  end
-
-  def perform_redirect_after_error(try_to_redirect_to_account_path = true)
-    account_id = nested_hash_value(params.permit!.to_h, :account_id)
-    if try_to_redirect_to_account_path && account_id.present?
-      redirect_to kaui_engine.account_path(account_id)
-    else
-      redirect_to kaui_engine.home_path
-    end
   end
 end
--- app/helpers/kaui/exception_helper.rb
+++ app/helpers/kaui/exception_helper.rb
@@ -3,12 +3,18 @@
 module Kaui
   module ExceptionHelper
     def standardize_exception(exception)
+      if defined?(JRUBY_VERSION)
+        case exception
+        when ActiveRecord::JDBCError
+          return I18n.translate('errors.messages.unable_to_connect_database')
+        end
+      end
+
       case exception
-      when ActiveRecord::DatabaseConnectionError, ActiveRecord::JDBCError
+      when ActiveRecord::DatabaseConnectionError
         I18n.translate('errors.messages.unable_to_connect_database')
-      when Errno::ECONNREFUSED
-        url = exception.message.match(/for "(.*)" port/) { |m| m[1] }
-        url && (KillBillClient.url.include? url) ? I18n.translate('errors.messages.unable_to_connect_killbill') : nil
+      when Errno::ECONNREFUSED, Errno::EBADF
+        I18n.translate('errors.messages.unable_to_connect_killbill')
       when ->(e) { e.class.name.start_with?('KillBillClient::API') }
         I18n.translate('errors.messages.error_communicating_killbill')
       else
