012af146ee654855e40d7b0d61c4e76a940d323e
piyushkalasariya <piyushkalasariya555@gmail.com>
2025-06-17 17:32:39 +0530

    Header CSS design updates

--- test/dummy/app/assets/config/manifest.js
+++ test/dummy/app/assets/config/manifest.js
@@ -1,2 +1,8 @@
 //= link kaui_manifest.js
 //= link routes.js
+//= link kaui/header/search_icon.svg
+//= link kaui/header/cog_icon.svg
+//= link kaui/header/account.svg
+//= link kaui/header/subscription.svg
+//= link kaui/header/payment.svg
+//= link kaui/header/invoice.svg
